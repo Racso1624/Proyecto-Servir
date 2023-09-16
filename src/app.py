b3bdb170eaabc5548b667722616b9bce7ffbc679
Racso1624 <fer10_lb@outlook.com>
2023-09-15 21:52:27 -0600

    Se conecta a la base de datos

--- src/app.py
+++ src/app.py
@@ -6,9 +6,25 @@ Prueba Técnica
 
 # Importar librerias necesarias
 from flask import Flask, render_template
+import psycopg2 
+import psycopg2.extras
+from dotenv import load_dotenv
+import os
+
+# Cargar informacion de la conexion a la base de datos
+load_dotenv()
 
 # Correr Flask para la pagina
 app = Flask(__name__)
+
+# Conexion a la base de datos que contiene la informacion
+database_connection = psycopg2.connect(
+    database = os.getenv("DATABASE"),
+    user = os.getenv("USER"),
+    password = os.getenv("PASSWORD"),
+    host = os.getenv("HOST"),
+    port = os.getenv("PORT")
+)
 
 # Crear la ruta principal
 @app.route('/')
